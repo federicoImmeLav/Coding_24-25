cff6f79ff726c86baa4c8f0e68e44e5583ae1aed
federicoImmeLav <117451142+federicoImmeLav@users.noreply.github.com>
2024-11-06 10:18:15 +0100

    calc def

--- 2D/7-datiJs/script.js
+++ 2D/7-datiJs/script.js
@@ -17,5 +17,89 @@ function somma() {
 
     // scrivo il risultato della somma nel campo che si chiama risultato 
     document.getElementById("risultato").value = c;
+
+    let d = document.createElement("p"); //creo un paragrafo
+    console.log(d);
+    let testo = document.createTextNode(a + "+" + b + "=" + c); //creo il testo per il paragrafo aggiungendo i vari pezzettini con le virgolette
+    console.log(testo);
+    // metto il testo dentro al paragrafo 
+    d.appendChild(testo);
+    console.log(d);
+    // prendo l'elemento che si chiama cronologia e ci metto dentro il pargrafo che si chiama d 
+    document.getElementById("cronologia").appendChild(d);
 }
 
+// creo la funzione per la sottrazione 
+function differenza() {
+    // leggo il valore nei due campi di input e li converto in numeri con parseInt 
+    let a = parseInt(document.getElementById("numero1").value);
+    let b = parseInt(document.getElementById("numero2").value);
+    // faccio la differenza tra i due numeri 
+    let c = a - b;
+
+     // scrivo il risultato della differenza nel campo che si chiama risultato 
+     document.getElementById("risultato").value = c;
+
+     let d = document.createElement("p"); //creo un paragrafo
+     console.log(d);
+     let testo = document.createTextNode(a + "-" + b + "=" + c); //creo il testo per il paragrafo aggiungendo i vari pezzettini con le virgolette
+     console.log(testo);
+     // metto il testo dentro al paragrafo 
+     d.appendChild(testo);
+     console.log(d);
+     // prendo l'elemento che si chiama cronologia e ci metto dentro il pargrafo che si chiama d 
+     document.getElementById("cronologia").appendChild(d);
+}
+
+// creo la funzione per il prodotto 
+function prodotto() {
+    // leggo il valore nei due campi di input e li converto in numeri con parseInt 
+    let a = parseInt(document.getElementById("numero1").value);
+    let b = parseInt(document.getElementById("numero2").value);
+    // faccio la moltiplicazione tra i due numeri 
+    let c = a * b;
+
+     // scrivo il risultato del prodotto nel campo che si chiama risultato 
+     document.getElementById("risultato").value = c;
+
+     let d = document.createElement("p"); //creo un paragrafo
+     console.log(d);
+     let testo = document.createTextNode(a + "X" + b + "=" + c); //creo il testo per il paragrafo aggiungendo i vari pezzettini con le virgolette
+     console.log(testo);
+     // metto il testo dentro al paragrafo 
+     d.appendChild(testo);
+     console.log(d);
+     // prendo l'elemento che si chiama cronologia e ci metto dentro il pargrafo che si chiama d 
+     document.getElementById("cronologia").appendChild(d);
+}
+
+// creo la funzione per la divisione 
+function quoziente() {
+    // leggo il valore nei due campi di input e li converto in numeri con parseInt 
+    let a = parseInt(document.getElementById("numero1").value);
+    let b = parseInt(document.getElementById("numero2").value);
+    // faccio la divisione tra i due numeri 
+    let c = a / b;
+
+     // scrivo il risultato della divisione nel campo che si chiama risultato 
+     document.getElementById("risultato").value = c;
+
+     let d = document.createElement("p"); //creo un paragrafo
+     console.log(d);
+     let testo = document.createTextNode(a + "/" + b + "=" + c); //creo il testo per il paragrafo aggiungendo i vari pezzettini con le virgolette
+     console.log(testo);
+     // metto il testo dentro al paragrafo 
+     d.appendChild(testo);
+     console.log(d);
+     // prendo l'elemento che si chiama cronologia e ci metto dentro il pargrafo che si chiama d 
+     document.getElementById("cronologia").appendChild(d);
+}
+
+
+// creo la funzione di reset 
+function reset() {
+    // devo prendermi tutti i paragrafi all'interno del div cronologia 
+   let q =  document.querySelector("#cronologia p");
+   console.log(q);
+   q.remove(); //tolgo l'elemento che ho selezionato
+}
