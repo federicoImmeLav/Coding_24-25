87dbd21ed275e021b0345e4374d3e0fa595d8dff
federicoImmeLav <117451142+federicoImmeLav@users.noreply.github.com>
2024-12-17 13:13:08 +0100

    aa

--- 2D/10-boolean/script.js
+++ 2D/10-boolean/script.js
@@ -15,3 +15,35 @@ document.getElementById("barra").innerHTML = `
             <div class="pulsante">
                 <button id="iscrivi">Iscriviti</button>
             </div>  `  
+
+// creo la funzione che ingrandisce il cerchio
+function cerchio() {
+    // cambio sia altezza che larghezza con la funzione
+    document.getElementById("cerchio").style.height = "40px";
+    document.getElementById("cerchio").style.width = "40px";
+    // aggiungo la transizione per l'animazione 
+    document.getElementById("cerchio").style.transition = "all 0.2s ease-in-out";
+}
+
+// creo la funzione che rimpicciolisce
+function cerchioPiccolo() {
+    // cambio sia altezza che larghezza con la funzione
+    document.getElementById("cerchio").style.height = "25px";
+    document.getElementById("cerchio").style.width = "25px";
+}
+
+// creo la funzione che ingrandisce il cerchio
+function cerchio2() {
+    // cambio sia altezza che larghezza con la funzione
+    document.getElementById("cerchio2").style.height = "40px";
+    document.getElementById("cerchio2").style.width = "40px";
+    // aggiungo la transizione per l'animazione 
+    document.getElementById("cerchio2").style.transition = "all 0.2s ease-in-out";
+}
+
+// creo la funzione che rimpicciolisce
+function cerchioPiccolo2() {
+    // cambio sia altezza che larghezza con la funzione
+    document.getElementById("cerchio2").style.height = "25px";
+    document.getElementById("cerchio2").style.width = "25px";
+}
