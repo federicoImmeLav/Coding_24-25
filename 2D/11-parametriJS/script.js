12b02edb4bd8bf918a12fba66bcde22551694da4
federicoImmeLav <117451142+federicoImmeLav@users.noreply.github.com>
2024-12-18 13:09:55 +0100

    parametri

---
+++ 2D/11-parametriJS/script.js
@@ -0,0 +1,14 @@
+// creo la funzione
+function quadratoColore(b) {
+    document.getElementById("quadrato").style.backgroundColor = b;
+}
+// dopo che ho fatto la funzione devo dire all'elemento che si può cliccare
+
+function cambiaTesto1() {
+    document.getElementById("testo").innerHTML = "Cane";
+}
+
+// uso le funzioni con i parametri
+function cambia(a) {
+    document.getElementById("testo").innerHTML = a;
+}
