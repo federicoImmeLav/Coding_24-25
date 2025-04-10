5ebd45c66e0fa75648d4cade72999b9364e5ca85
federicoImmeLav <117451142+federicoImmeLav@users.noreply.github.com>
2025-04-10 12:22:07 +0200

    lezioni 3D

--- 1D/12-baseJavascript/script.js
+++ 1D/12-baseJavascript/script.js
@@ -28,3 +28,32 @@ function cambiaMenu() {
     }
 }
 
+// creo la funzione che scrive nel paragrafo 
+function prova() {
+    // prendo il paragrafo in cui voglio scrivere
+    let paragrafo = document.getElementById("risultato");
+
+    // scrivo nel paragrafo 
+    paragrafo.innerHTML = "Ciao";
+}
+
+// creo la funzione che legge nome e cognome e li unisce
+// in una frase 
+function nomeCognome() {
+    // leggo quello che c'è scritto in nome 
+    let nome = document.getElementById("nome").value;
+    // leggo quello che c'è scritto in cognome
+    let cognome = document.getElementById("cognome").value;
+    // scrivo in risultato2 nome e cognome insieme 
+    document.getElementById("risultato2").innerHTML = "Ti chiami " + nome + " " + cognome;
+}
+
+// creo la funzione che somma i due valori che inserisco 
+function somma() {
+    // leggo il primo numero 
+    let numero1 = parseInt(document.getElementById("numero1").value);
+    // leggo il secondo numero 
+    let numero2 = parseInt(document.getElementById("numero2").value);
+    // sommo e scrivo il risultato
+    document.getElementById("somma").innerHTML = numero1 + numero2;
+}
